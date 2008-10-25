30fb11e53278a555e967f5f06f10ec907b7abd17
andrewb <andrewb@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-10-25 00:58:31 +0000

    swap out the dummy export context for favor of the tarball context, our form continues as the export context and we adapt the form to IFilesystemExporter so that we can export the contained data

--- Products/PloneFormGen/browser.py
+++ Products/PloneFormGen/browser.py
@@ -1,7 +1,9 @@
 from zope.interface import implements
 from Products.Five import BrowserView
 from Products.PloneFormGen.interfaces import IFormFolderExportView
-from Products.GenericSetup.tests.common import DummyExportContext
+
+from Products.GenericSetup.context import TarballExportContext
+from Products.GenericSetup.interfaces import IFilesystemExporter
 
 class FormFolderExportView(BrowserView):
     implements(IFormFolderExportView)
@@ -9,7 +11,14 @@ class FormFolderExportView(BrowserView):
     def __call__(self):
         """ See IFormFolderExportView.
         """
-        return DummyExportContext(self.context)
+        ctx = TarballExportContext(self.context)
         
-
+        self.request.RESPONSE.setHeader('Content-type', 'application/x-gzip')
+        self.request.RESPONSE.setHeader('Content-disposition',
+                           'attachment; filename=%s' % ctx.getArchiveFilename())
         
+        # export the structure treating the current form as our root context
+        IFilesystemExporter(self.context).export(ctx, 'structure', True)
+        
+        return ctx.getArchive()
+    
