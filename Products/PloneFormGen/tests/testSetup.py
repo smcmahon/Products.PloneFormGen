10957f309ea4e9991842dd3996962c287bebdf20
andrewb <andrewb@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-11-15 00:25:49 +0000

    changing action category for export/import actions, adding failing test for new import action

--- Products/PloneFormGen/tests/testSetup.py
+++ Products/PloneFormGen/tests/testSetup.py
@@ -88,7 +88,8 @@ class TestInstallation(pfgtc.PloneFormGenTestCase):
     
     def testActionsInstalled(self):
         ff_fti = self.types['FormFolder']
-        self.failUnless(ff_fti.getActionObject('document_actions/export'))
+        self.failUnless(ff_fti.getActionObject('object_buttons/export'))
+        self.failUnless(ff_fti.getActionObject('object_buttons/import'))
     
     def testPortalFactorySetup(self):
         for f in self.metaTypes:
