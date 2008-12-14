b1e28d15efd4b102ea645074b9f30427e469f02b
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-12-14 20:19:47 +0000

    fix the test that server side fields aren't rendered

--- Products/PloneFormGen/tests/testDocTests.py
+++ Products/PloneFormGen/tests/testDocTests.py
@@ -4,20 +4,21 @@ import doctest
 from Testing import ZopeTestCase as ztc
 from Products.PloneFormGen.tests.pfgtc import PloneFormGenFunctionalTestCase
 
+suites = (
+    'browser.txt',
+    'serverside_field.txt',
+)
+
 def test_suite():
     return unittest.TestSuite([
 
         ztc.FunctionalDocFileSuite(
-            'browser.txt', package='Products.PloneFormGen.tests',
+            f, package='Products.PloneFormGen.tests',
             test_class=PloneFormGenFunctionalTestCase,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE | doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
-
-        ztc.FunctionalDocFileSuite(
-            'browser_serverside_test.txt', package='Products.PloneFormGen.tests',
-            test_class=PloneFormGenFunctionalTestCase,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE | doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
-
-        ])
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE | doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
+        
+            for f in suites]
+        )
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
