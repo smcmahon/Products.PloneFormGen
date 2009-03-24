7a4d29fed8e7ea1405e39cbf5f6325234f6502a9
smcmahon <smcmahon@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-03-24 18:41:16 +0000

    Fix broken msg compiler, fix de, update .mo files

--- Products/PloneFormGen/compile-locales.sh
+++ Products/PloneFormGen/compile-locales.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Compile .po files present in locales directory
 
-find ./locales/ -name *.po | sed -e 's/\(.*\)\.po/\/usr\/bin\/msgfmt -o \1.mo \1.po/' | xargs -J "cmd"
+find ./locales -name *.po | sed -e 's/\(.*\)\.po/-o \1.mo \1.po/' | xargs -t -n 3 msgfmt
 
