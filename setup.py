701cb9792a367a1b552b53c0932a789b6eab0c0f
claytron <claytron@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-04-15 15:12:06 +0000

    Added the CHANGES.txt to the long_description so that it show up on pypi, Hello World!  Also cleaned up the reST so that it will render properly

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ version = open(os.path.join("Products", "PloneFormGen", "version.txt")).read().s
 setup(name='Products.PloneFormGen',
       version=version,
       description="A through-the-web form generator for Plone",
-      long_description=open(os.path.join("Products", "PloneFormGen", "README.txt")).read(),
+      long_description=open(os.path.join("Products", "PloneFormGen", "README.txt")).read() \
+                       + "\n\n" + \
+                       open(os.path.join("Products", "PloneFormGen", "CHANGES.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
