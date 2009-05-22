c059f3a8e7a4b95bbb7cb04207bf67da42d60f9a
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-05-22 16:34:35 +0000

    failing test demonstrating http://plone.org/products/ploneformgen/issues/247

--- Products/PloneFormGen/tests/testEvents.py
+++ Products/PloneFormGen/tests/testEvents.py
@@ -35,6 +35,14 @@ class TestAdapterPaste(pfgtc.PloneFormGenTestCase):
             
             self.failUnless(new_id in self.ff1.getActionAdapter())
     
+    def testActiveAdaptersNotDuplicatedOnFormCopy(self):
+        self.loginAsPortalOwner()
+        copy = self.folder.manage_copyObjects('ff1')
+        new_id = self.folder.manage_pasteObjects(copy)[0]['new_id']
+        ff2 = getattr(self.folder, new_id)
+        active_adapters = ff2.getActionAdapter()
+        self.assertEqual(active_adapters, ('mailer',))
+    
     def testTempFactoryAdaptersNotActivatedOnForm(self):
         self.loginAsPortalOwner() # creating a FormCustomScriptAdapter requires elevated permissions
         adapter_count = len(self.ff1.getActionAdapter())
@@ -43,9 +51,6 @@ class TestAdapterPaste(pfgtc.PloneFormGenTestCase):
         # temporary objects shouldn't be active on the form folder
         self.assertEqual(adapter_count, len(self.ff1.getActionAdapter()))
         
-
-if  __name__ == '__main__':
-    framework()
 
 def test_suite():
     from unittest import TestSuite, makeSuite
