e9348538b4b647dbf847d0bb562858b6407e5936
mattss <mattss@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-10 15:35:40 +0000

    If the form is submitted, add the user to the query string and redirect back to the main form.

--- Products/PloneFormGen/browser/chooseUser.py
+++ Products/PloneFormGen/browser/chooseUser.py
@@ -12,6 +12,12 @@ class ChooseUserView(BrowserView):
     def update(self):
         self.all_users = self.context.portal_membership.listMembers()
 
+        # If the user form has been submitted, send the user back to the main form
+        if self.request['REQUEST_METHOD'] == 'POST':
+            override_key = self.request.form.get('user-select', '')
+            url = self.context.absolute_url() 
+            self.request.response.redirect(url + '?override_key=' + override_key)
+
     def __call__(self):
         self.update()
         return super(ChooseUserView, self).__call__()
