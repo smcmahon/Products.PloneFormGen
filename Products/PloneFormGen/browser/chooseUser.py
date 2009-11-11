a4fef7cf292ab143369f4970b98d29e8524acb65
mattss <mattss@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-11 11:03:47 +0000

    Try to look up a user based on a search string. If some are found, look up their fullnames and display them as radio buttons. These can then be chosen so that the form may be submitted as that user.

--- Products/PloneFormGen/browser/chooseUser.py
+++ Products/PloneFormGen/browser/chooseUser.py
@@ -10,13 +10,32 @@ class ChooseUserView(BrowserView):
     """
 
     def update(self):
-        self.all_users = self.context.portal_membership.listMembers()
-
-        # If the user form has been submitted, send the user back to the main form
+        self.users = []
+        self.no_users = ''
+        self.raw_users = []
+        
+        # Work out which form was submitted and try and look up the user
         if self.request['REQUEST_METHOD'] == 'POST':
-            override_key = self.request.form.get('user-select', '')
-            url = self.context.absolute_url() 
-            self.request.response.redirect(url + '?override_key=' + override_key)
+            if self.request.form.get('user-search'):
+                query = self.request.form.get('user-search')                
+                self.raw_users = self.context.portal_membership.searchMembers('fullname', query)
+                # if we didn't get users, maybe we need to captialise the string
+                if not self.raw_users:
+                    self.raw_users = self.context.portal_membership.searchMembers('fullname', query.title())
+                if not self.raw_users:
+                    self.no_users = 'No users found'
+            elif self.request.form.get('user-select'):
+                override_key = self.request.form.get('user-select', '')
+                url = self.context.absolute_url() 
+                self.request.response.redirect(url + '?override_key=' + override_key)
+                
+        # if there were some users looked up, get their full names
+        if self.raw_users:
+            for user in self.raw_users:
+                user_ob = self.context.portal_membership.getMemberById(user['username'])
+                user_fullname = user_ob.getProperty('fullname')
+                user['fullname'] = user_fullname
+                self.users.append(user)
 
     def __call__(self):
         self.update()
