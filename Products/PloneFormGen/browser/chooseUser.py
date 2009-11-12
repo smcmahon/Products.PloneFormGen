2af74f6b90cd47c660565c489cf7fdcf8b4b61f9
bigbadben <bigbadben@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-12 11:25:41 +0000

    updated the choose user logic - if you are a Manager you can search for another user to submit the form on behalf of

--- Products/PloneFormGen/browser/chooseUser.py
+++ Products/PloneFormGen/browser/chooseUser.py
@@ -11,31 +11,42 @@ class ChooseUserView(BrowserView):
 
     def update(self):
         self.users = []
-        self.no_users = ''
-        self.raw_users = []
-        
+        self.no_users = False
+
         # Work out which form was submitted and try and look up the user
         if self.request['REQUEST_METHOD'] == 'POST':
             if self.request.form.get('user-search'):
-                query = self.request.form.get('user-search')                
-                self.raw_users = self.context.portal_membership.searchMembers('fullname', query)
-                # if we didn't get users, maybe we need to captialise the string
-                if not self.raw_users:
-                    self.raw_users = self.context.portal_membership.searchMembers('fullname', query.title())
-                if not self.raw_users:
-                    self.no_users = 'No users found'
+                query = self.request.form.get('user-search').lower() 
+                portal = self.context.portal_url.getPortalObject()
+                pmt = portal.portal_membership
+                if query:
+                    if getattr(portal, 'membrane_tool', None):
+                        # membrane
+                        memberbrains = portal.membrane_tool.searchResults(
+                            SearchableText=query,
+                            )
+                        for b in memberbrains:
+                            m = pmt.getMemberById(b['getUserId'])
+                            if m:
+                                self.users.append(m)
+                    else:
+                        # SLOW
+                        # in our site, portal_membership.searchMembers doesn't
+                        # help us - someone else might be able to figure this
+                        # out
+                        members = pmt.listMembers()
+                        for m in members:
+                            fn = m.getProperty('fullname')
+                            un = m.getProperty('userName')
+                            if fn.lower().find(query) > -1 \
+                                    or un.lower().find(query) > -1:
+                                self.users.append(m)
+                    if not self.users:
+                        self.no_users = True
             elif self.request.form.get('user-select'):
                 override_key = self.request.form.get('user-select', '')
                 url = self.context.absolute_url() 
                 self.request.response.redirect(url + '?override_key=' + override_key)
-                
-        # if there were some users looked up, get their full names
-        if self.raw_users:
-            for user in self.raw_users:
-                user_ob = self.context.portal_membership.getMemberById(user['username'])
-                user_fullname = user_ob.getProperty('fullname')
-                user['fullname'] = user_fullname
-                self.users.append(user)
 
     def __call__(self):
         self.update()
