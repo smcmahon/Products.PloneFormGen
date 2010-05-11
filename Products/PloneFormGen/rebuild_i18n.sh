a39d452ded4d0a61d71b64fd5dc181246a55b7f5
toutpt <toutpt@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-05-11 10:11:22 +0000

    add rebuild_i18n script

---
+++ Products/PloneFormGen/rebuild_i18n.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+PRODUCTNAME='ploneformgen'
+I18NDOMAIN='Products.PloneFormGen'
+
+# Synchronise the .pot with the templates.
+i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
+
+# Synchronise the resulting .pot with the .po files
+i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
+
+
