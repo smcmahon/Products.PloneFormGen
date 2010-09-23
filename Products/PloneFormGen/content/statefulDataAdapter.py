fbfc323e8e68d20d1454e2e9bc3bc20580b88a57
mikerhodes <mikerhodes@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-09-23 11:47:12 +0000

    Carglass Belgium issue 71: Handle unicode data in StatefulDataAdaptor

--- Products/PloneFormGen/content/statefulDataAdapter.py
+++ Products/PloneFormGen/content/statefulDataAdapter.py
@@ -173,7 +173,7 @@ class FormStatefulDataAdapter(FormActionAdapter):
         delimiter = self.getFieldDelimiter()
 
         output = StringIO()
-        csv_writer = writer(output, delimiter=delimiter)
+        csv_writer = UnicodeWriter(output, delimiter=delimiter)
 
         #The first column should always be the user
         head_row = ['User']
@@ -303,5 +303,37 @@ class FormStatefulDataAdapter(FormActionAdapter):
             final_counts[data_type] = count
                     
         return final_counts
+
+
+import csv, codecs, cStringIO
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getencoder(encoding)
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder(data)[0]
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 registerATCT(FormStatefulDataAdapter, PROJECTNAME)
