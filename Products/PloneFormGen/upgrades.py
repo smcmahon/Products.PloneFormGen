4c66c18301484fc4c63aefda51886b54cb3588eb
toutpt <toutpt@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-01-07 23:17:58 +0000

    - Use collective.js.jqueryui instead of an embeded version custom of jqueryui.   make it easy to integrate with other add-ons.   Switch profile version from the egg version to an integer to increase with   each changes done on the profile.

--- Products/PloneFormGen/upgrades.py
+++ Products/PloneFormGen/upgrades.py
@@ -1,3 +1,10 @@
+from Products.CMFCore.utils import getToolByName
+
 def null_upgrade_step(tool):
     """ This is a null upgrade, use it when nothing happens """
     pass
+
+def upgrade_to_170(context):
+    #apply the new dependency c.js.jqueryui
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-collective.js.jqueryui:default')
