cda627322f6002996e4e8b816297b4b1653e17f8
toutpt <toutpt@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-04-02 21:55:06 +0000

    Add collective.portlet.itemview integration.

---
+++ Products/PloneFormGen/portlet/itemview.py
@@ -0,0 +1,14 @@
+from Products.Five import BrowserView
+
+class FormGenView(BrowserView):
+    """BrowserView"""
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def title(self):
+        return self.context.Title()
+
+    def embedded_form(self):
+        return self.context.restrictedTraverse('fg_embedded_view_p3')()
