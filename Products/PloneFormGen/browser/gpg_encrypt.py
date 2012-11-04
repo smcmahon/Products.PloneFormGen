36142482e11bf9262e407edc95423de6023c855f
Steve McMahon <steve@dcn.org>
2012-11-04 15:08:01 -0800

    Add a gpg encryption browser view

---
+++ Products/PloneFormGen/browser/gpg_encrypt.py
@@ -0,0 +1,16 @@
+"""
+gpg_encrypt
+Provides a browser view with services from
+ya_gpg.
+"""
+
+from Products.Five import BrowserView
+from Products.PloneFormGen.content.ya_gpg import gpg
+
+
+class GnuPGView(BrowserView):
+
+    def encrypt(self, data, recipient_key_id):
+        "Encrypt the message contained in the string 'data'"
+
+        return gpg.encrypt(data, recipient_key_id)
