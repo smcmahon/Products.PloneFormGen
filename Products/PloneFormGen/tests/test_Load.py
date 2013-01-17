366654ff0f90e3b0d8b23be9dccaca329eb3f6a1
Maurits van Rees <maurits@vanrees.org>
2013-01-17 16:35:48 +0100

    Ignore load tests when collective.funkload is unavailable.
    Otherwise you get an error, which makes the -D argument to bin/test useless.

--- Products/PloneFormGen/tests/test_Load.py
+++ Products/PloneFormGen/tests/test_Load.py
@@ -1,34 +1,44 @@
 import unittest
-from collective.funkload import testcase
+try:
+    from collective.funkload import testcase
+    haveFunkload = True
+except ImportError:
+    haveFunkload = False
+    print "collective.funkload is unavailable. Ignoring load tests."
 
-class PFGLoadTest(testcase.FLTestCase):
-    """Let's see how the saved data adapter performs under load..."""
 
-    def setUp(self):
-        self.logd("setUp")
-        self.label = 'PFG load test'
-        self.server_url = self.conf_get('main', 'url')
+if haveFunkload:
+    class PFGLoadTest(testcase.FLTestCase):
+        """Let's see how the saved data adapter performs under load..."""
 
-    def test_submitForm(self):
-        self.get(self.server_url + '/testform',
-                 description = 'Load form')
+        def setUp(self):
+            self.logd("setUp")
+            self.label = 'PFG load test'
+            self.server_url = self.conf_get('main', 'url')
 
-        self.post(
-            self.server_url + '/testform',
-            params = [
-                ['replyto', 'test@example.com'],
-                ['topic', 'test'],
-                ['comments', 'test'],
-                ['form.submitted', 1],
-                ],
-            description = 'Submit form'
-            )
+        def test_submitForm(self):
+            self.get(self.server_url + '/testform',
+                     description = 'Load form')
 
-    def tearDown(self):
-        self.logd('tearDown')
+            self.post(
+                self.server_url + '/testform',
+                params = [
+                    ['replyto', 'test@example.com'],
+                    ['topic', 'test'],
+                    ['comments', 'test'],
+                    ['form.submitted', 1],
+                    ],
+                description = 'Submit form'
+                )
+
+        def tearDown(self):
+            self.logd('tearDown')
+
 
 def test_suite():
-    return unittest.makeSuite(PFGLoadTest)
+    if haveFunkload:
+        return unittest.makeSuite(PFGLoadTest)
+    return unittest.TestSuite()
 
 additional_tests = test_suite
 
