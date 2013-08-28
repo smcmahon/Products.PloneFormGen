efef5aadc287c44036305bd1e8136ac58ed3d603
Tom Gross <itconsense@gmail.com>
2013-08-28 17:09:34 +0200

    allow translation of validators on a modern Plone

--- Products/PloneFormGen/rebuild_i18n.sh
+++ Products/PloneFormGen/rebuild_i18n.sh
@@ -3,9 +3,9 @@ PRODUCTNAME='ploneformgen'
 I18NDOMAIN='ploneformgen'
 
 # Synchronise the .pot with the templates.
-/opt/python/python-2.6/bin/i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
+i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
 
 # Synchronise the resulting .pot with the .po files
-/opt/python/python-2.6/bin/i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
+i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
 
 
