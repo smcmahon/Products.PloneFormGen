fdd56231c0407ba1cfc647c915c7a40939f74f3c
Steve McMahon <steve@dcn.org>
2014-01-20 10:31:59 -0800

    Manually merge 'fix incompatibility with LinguaPlone' pull request

--- Products/PloneFormGen/content/fieldset.py
+++ Products/PloneFormGen/content/fieldset.py
@@ -18,7 +18,7 @@ from Products.CMFPlone.utils import safe_hasattr, base_hasattr
 
 from Products.Archetypes.public import *
 
-from plone.app.folder.folder import ATFolderSchema, ATFolder
+from Products.ATContentTypes.content.folder import ATFolderSchema, ATFolder
 
 from Products.ATContentTypes.content.schemata import finalizeATCTSchema
 from Products.ATContentTypes.content.base import registerATCT
