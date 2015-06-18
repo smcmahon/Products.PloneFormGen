08698717aa29d9c004f37c76438d24174aa494c5
Steve McMahon <steve@dcn.org>
2015-06-18 15:34:58 -0700

    toolbox positioning

--- Products/PloneFormGen/browser/resources/quickedit.js
+++ Products/PloneFormGen/browser/resources/quickedit.js
@@ -5,3 +5,22 @@
 /*jslint white: true, browser: true, onevar: false, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, regexp: false */
 
 var pfgQEdit = {};
+
+jQuery(function ($) {
+
+    // initial styling
+
+    $(".ArchetypesCaptchaWidget .captchaImage").replaceWith("<div>" + pfgQEdit.messages.NO_CAPTCHA_MSG + "</div>");
+
+    // disable and dim input elements
+    $("#pfg-fieldwrapper .field :input")
+        .css('opacity', 0.5)
+        .each(function () {
+            if (typeof this.disabled !== "undefined") {
+                this.disabled = true;
+            }
+        });
+
+    $("#allWidgets").tabs(".widgetPane", {tabs:"h2", effect:'slide'});
+
+});
