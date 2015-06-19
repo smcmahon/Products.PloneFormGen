8591313d7aeeecc29968e4eb973c21aa0f30ffa2
Steve McMahon <steve@dcn.org>
2015-06-18 18:03:01 -0700

    Target is P5

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name='Products.PloneFormGen',
       install_requires=[
           'setuptools',
           'Products.Archetypes>=1.7.14',  # placeholder support
-          'Products.CMFPlone>=5.0b3.dev0',
+          'Products.CMFPlone>=5.0b2.dev0',
           'Products.TALESField>=1.1.3',
           'Products.TemplateFields>=1.2.4',
           'Products.PythonField>=1.1.3',
