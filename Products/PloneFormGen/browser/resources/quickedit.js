ff17f4fcdbad6d0a4a720ca47c52be4acf52f35d
Steve McMahon <steve@dcn.org>
2015-07-02 14:47:45 -0700

    Add toolbox, action d&d

--- Products/PloneFormGen/browser/resources/quickedit.js
+++ Products/PloneFormGen/browser/resources/quickedit.js
@@ -23,21 +23,55 @@ requirejs(['jquery', 'jquery.event.drag', 'jquery.event.drop'], function ($, dra
     $("#allWidgets").tabs(".widgetPane", {tabs: "h2", effect: 'slide'});
 
     var dd_defaults = {
+        drag_selector: null,
+        drop_selector: null,
         dragClass: 'item-dragging',
         cloneClass: 'drag-proxy',
         drop: null // function to handle drop event
         };
 
-    function dd_init(drag_selector, drop_selector, opts) {
-        var options = $.extend({}, dd_defaults, opts);
+    function dd_init(config) {
+        var options = $.extend({}, dd_defaults, config),
+            last_target,
+            drop_targets;
 
-        $(drag_selector)
+        $(options.drag_selector)
             .drag("start", function(ev, dd) {
+
+                // drop is a global; we need to use it here so that our configuration
+                // takes control from any previous settings.
+                drop({
+                    tolerance: function(event, proxy, target) {
+                        var test = event.pageY > ( target.top + target.height / 2 );
+
+                        $.data( target.elem, "drop+reorder", test ? "insert_after" : "insert_before" );
+                        return this.contains(target, [ event.pageX, event.pageY ]);
+                        }
+                    });
+
+                $(this).addClass(options.dragClass);
+
+                // create and return a proxy
                 return $(this).clone()
                     .addClass(options.cloneClass)
                     .appendTo($('body'));
             })
-            .drag(function(ev, dd){
+            .drag(function(ev, dd) {
+                var drop = dd.drop[0],
+                    method = $.data( drop || {}, "drop+reorder" );
+
+                if (!drop) {
+                    drop_targets.removeClass("insert_before insert_after");
+                }
+
+                if (drop && (drop != dd.current || method != dd.method)){
+                    drop_targets.removeClass("insert_before insert_after");
+                    $(drop).addClass(method);
+                    dd.current = drop;
+                    dd.method = method;
+                    dd.update();
+                }
+                // move the proxy
                 $(dd.proxy).css({
                     top: dd.offsetY,
                     left: dd.offsetX
@@ -45,12 +79,52 @@ requirejs(['jquery', 'jquery.event.drag', 'jquery.event.drop'], function ($, dra
             })
             .drag("end",function(ev, dd){
                 $(dd.proxy).remove();
+                $(this).removeClass(options.dragClass);
+                drop_targets.removeClass("insert_before insert_after");
             });
-        // $(drop_selector).drop(function(ev, dd){
-        //     console.log('drop');
-        // });
+
+        drop_targets = $(options.drop_selector);
+        drop_targets
+            .drop("init",function( ev, dd ){
+                return !( this == dd.drag );
+            })
+            .drop(function(ev, dd){
+                if (options.drop) {
+                    options.drop(dd);
+                }
+            });
     }
 
-    dd_init('.qefield', '.qefield', {});
+    dd_init({
+        drag_selector: '#pfg-qetable .qefield',
+        drop_selector: '#pfg-qetable .qefield',
+        drop: function (dd, target) {
+            console.log(dd, 'drop');
+        }
+    });
+
+    dd_init({
+        drag_selector: '#fieldWidgets .widget, #fieldsetWidgets .widget',
+        drop_selector: '#pfg-qetable .qefield',
+        drop: function (dd, target) {
+            console.log(dd, 'drop');
+        }
+    });
+
+    dd_init({
+        drag_selector: '#pfgActionEdit .qefield',
+        drop_selector: '#pfgActionEdit .qefield',
+        drop: function (dd, target) {
+            console.log(dd, 'drop');
+        }
+    });
+
+    dd_init({
+        drag_selector: '#actionWidgets .widget',
+        drop_selector: '#pfgActionEdit .qefield',
+        drop: function (dd, target) {
+            console.log(dd, 'drop');
+        }
+    });
 
 });
