56b44725669f0739a96a77a477b56fd70da48a8c
Tom Gross <itconsense@gmail.com>
2016-05-09 13:14:22 +0000

    fix date validator to work with new pattern widget output

--- Products/PloneFormGen/content/fields.py
+++ Products/PloneFormGen/content/fields.py
@@ -572,7 +572,6 @@ class FGDateField(BaseFormField):
         tool = getToolByName(self, 'translation_service')
         return tool.ulocalized_time(time, long_format=long_format)
 
-
     def htmlValue(self, REQUEST):
         """ return from REQUEST, this field's value, rendered as XHTML.
         """
@@ -595,23 +594,19 @@ class FGDateField(BaseFormField):
 
         return cgi.escape(value)
 
-
     def specialValidator(self, value, field, REQUEST, errors):
         """ Archetypes isn't validating non-required dates --
             so we need to.
         """
-
         fname = field.getName()
-        month = REQUEST.form.get('%s_month'%fname, '01')
-        day = REQUEST.form.get('%s_month'%fname, '01')
-
-        if (month == '00') and (day == '00'):
-            value = ''
-            REQUEST.form[fname] = ''
-
         if value and not field.required:
             try:
-                dt = DateTime(value)
+                DateTime(value)
+            except (DateTimeSyntaxError, DateError):
+                return "Validation failed(isValidDate): this is not a valid date."
+        elif not value:
+            try:
+                DateTime(REQUEST.form.get(fname))
             except (DateTimeSyntaxError, DateError):
                 return "Validation failed(isValidDate): this is not a valid date."
         return 0
